0ebf01654588167d38360dce4a293c2d85e30e67
barteq100 <bartosz.o.blue@gmail.com>
2020-08-10 18:52:54 +0200

    AutoHook Sliders

--- Assets/SliderController.cs
+++ Assets/SliderController.cs
@@ -8,8 +8,19 @@ public class SliderController : MonoBehaviour
 {
     public Text ValueText;
     public Slider Slider;
+
+    public void Start()
+    {
+        ValueText.text = GetValueText(Slider.value);
+        Slider.onValueChanged.AddListener(OnSliderValueChange);
+    }
     public void OnSliderValueChange(float value)
     {
-        ValueText.text = ((int)(value * 100)).ToString();
+        ValueText.text = GetValueText(value);
+    }
+
+    private string GetValueText(float value)
+    {
+        return ((int)(value * 100)).ToString();
     }
 }
